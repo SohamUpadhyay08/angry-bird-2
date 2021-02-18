8d709de6752f4e7118f82279f019ec65f729ee1a
SohamUpadhyay08 <77781974+SohamUpadhyay08@users.noreply.github.com>
2021-02-18 16:44:12 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,16 @@
+class Bird extends BaseClass{
+    constructor(x, y) {
+      super(x,y,50,50);
+      
+      this.image = loadImage("sprites/bird.png");
+      
+    };
+    display(){
+    var position = this.body.position;
+    position.x = mouseX;
+    position.y = mouseY;
+    super.display();
+    };
+    
+  };
+  
